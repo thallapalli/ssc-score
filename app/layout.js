1475447564be575d06b065259ecb530f89973dc2
karnakar <thallapallikarnakar@gmail.com>
2026-04-05 23:31:29 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,9 @@
-export const metadata = {
-  title: 'SSC SCORE',
-  description: 'Data Management System',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, backgroundColor: '#000' }}>{children}</body>
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#000' }}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
